08f6759995674a7c378c913ee1f8475341c3be97
mifodiy911 <mifodiy911@gmail.com>
2024-02-19 18:58:48 +0200

    add logic to timer

--- src/Components/timer/Timer.js
+++ src/Components/timer/Timer.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Timer.css'
+import { getTimeToTimer } from '../../helpers/getTimeToTimer'
 
-const Timer = () => {
+const Timer = ({startDate}) => {
+	const [time, setTime] = useState(getTimeToTimer(startDate));
+	const {days, hours, minutes, seconds} = time;
+	const [isTripStarted, setIsTripStarted] = useState(false);
+	const isTimeExist = !days && !hours && !minutes && !seconds;
+
+	useEffect(() => {
+		if(!isTimeExist) {
+			setIsTripStarted(false)
+		} else {
+			setIsTripStarted(true)
+		}
+	}, [isTimeExist])
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setTime(getTimeToTimer(startDate))
+		},1000)
+
+		return () => clearInterval(timer)
+	}, [startDate])
 
 	return (
-		<ul className='timer'>
-			<li className='timer__item'>
-				<span className='timer__num'>01</span>
-				<span className='timer__label'>Days</span>
-			</li>
-			<li className='timer__item'>
-				<span className='timer__num'>01</span>
-				<span className='timer__label'>Hours</span>
-			</li>
-			<li className='timer__item'>
-				<span className='timer__num'>01</span>
-				<span className='timer__label'>Minutes</span>
-			</li>
-			<li className='timer__item'>
-				<span className='timer__num'>01</span>
-				<span className='timer__label'>Seconds</span>
-			</li>
-		</ul>
+		<div className='timer'>{
+			isTripStarted ? (
+				<div className='timer__message'>Your trip has already begun</div>
+			) : (
+				<ul className='timer__list'>
+					<li className='timer__item'>
+						<span className='timer__num'>{days}</span>
+						<span className='timer__label'>Days</span>
+					</li>
+					<li className='timer__item'>
+						<span className='timer__num'>{hours}</span>
+						<span className='timer__label'>Hours</span>
+					</li>
+					<li className='timer__item'>
+						<span className='timer__num'>{minutes}</span>
+						<span className='timer__label'>Minutes</span>
+					</li>
+					<li className='timer__item'>
+						<span className='timer__num'>{seconds}</span>
+						<span className='timer__label'>Seconds</span>
+					</li>
+				</ul>
+			)
+		}	
+		</div>
 	)
 }
 
